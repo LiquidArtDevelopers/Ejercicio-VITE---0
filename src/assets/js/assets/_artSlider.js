1622c6f4414c07c53852fd04232778f7678ecb34
MDMGN <michaelmdvrhack@gmail.com>
2026-06-19 14:02:32 +0200

    Refactor artSlider implementation and enhance styling for carousel navigation

--- src/assets/js/assets/_artSlider.js
+++ src/assets/js/assets/_artSlider.js
@@ -3,11 +3,10 @@ export default function artSlider() {
 
   $carrousel.forEach(($el) => {
     const $pista = $el.querySelector(".artSlider01__pista");
-
-    const $sliders = $pista.querySelectorAll(".artSlider01__slide");
-
+    const $dots = $el.querySelectorAll(".artSlider01__track__dot");
+    const $slides = $pista.querySelectorAll(".artSlider01__slide");
     // Comprobar que nuestra pista y los slider existan dentro del DOM
-    if (!$pista || $sliders.length === 0) return;
+    if (!$pista || $slides.length === 0) return;
 
     // contador guarda en que slide estamos.
     // Empieza en 0 porque la primera slide es la posicion inicial.
@@ -17,15 +16,43 @@ export default function artSlider() {
     const tiempoTransicion = 500;
     const tiempoEspera = 3000;
 
-    $sliders.forEach(($slide) => {
-      console.log($slide);
+    $slides.forEach(($slide) => {
+      const $copia = $slide.cloneNode(true);
+      $pista.appendChild($copia);
     });
 
+    // El setInterval se ejecutará cada 3000 milisegundos - tiempoEspera
     setInterval(() => {
-      console.log(`Contador: ${contador}`);
-      console.log(`Tiempo: ${(tiempoEspera * contador) / 1000}s`);
-      // contador = contador + 1;
       contador++;
+      $pista.style.transition = `transform ${tiempoTransicion}ms`;
+      $pista.style.transform = `translateX(-${100 * contador}%)`;
+      //Actual elemento dot "activo"
+      $dots[contador - 1]?.classList.remove("active");
+
+      // Siguiente elemento dot
+      if ($dots[contador]) {
+        // Comprobamos que nuestro dot no contenga la clase "active"
+        if (!$dots[contador].classList.contains("active")) {
+          $dots[contador].classList.add("active");
+        }
+      } else {
+        $dots[0].classList.add("active");
+      }
+
+      // Hay que validar el contador se igual al número de la última posición de slides
+      if (contador === $slides.length) {
+        setTimeout(() => {
+          // Reiniciar la posición del visor para mostrar el primer slide
+          $pista.style.transition = "none";
+          $pista.style.transform = `translateX(0)`;
+          contador = 0;
+        }, tiempoTransicion);
+      }
     }, tiempoEspera);
+
+    //
+    /* setTimeout(() => {
+      console.log(`Me ejecuto cada ${tiempoEspera}s`);
+    }, tiempoEspera); */
   });
 }
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,11 +1,11 @@
 import "../scss/main.scss"; //aquí importo el scss
 import "./assets/_artJs.js";
+import artSlider from "./assets/_artSlider.js";
 //aquí importo todo el código en bruto del otro fichero
 
 // Aquí importo la función que en el otro archivo exporto a través de un alias
 // Para poder ejecutar el código aquí importado, necesitaré ejecutar o kllamar a su función. (que es la función que en el otro fichero exporto)
 import { artAcordeon01 } from "./assets/_artAcordeon01.js";
-import artSlider from "./assets/_artSlider.js";
 
 //así llamo a la función que me viene del import, y lo hacemos cuando el dom se ha cargado. esto es un listener que espera a que el dom (html) se cargue.
 
